a8f4823e32dac8cdfcac0d31e17f5de8d82b082d
Kyle Klaiber <Kylesklaiber@gmail.com>
2018-10-02 16:01:39 -0700

    Passport JWT Authentication Strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 //Load User model
 const User = require("../../models/User");
@@ -68,19 +69,36 @@ router.post("/login", (req, res) => {
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; //create JWT payload
         //Sign Token
         jwt.sign(
-            payload, 
-            keys.secretOrKey, 
-            { expiresIn: 3600 }, 
-            (err, token) => {
-                res.json({
-                    success: true,
-                    token: 'Bearer ' + token
-                })
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
             });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password Incorrect" });
       }
     });
   });
 });
+
+//@route   GET api/users/current
+//@desc    Return Current User
+//@access  Private
+router.get(
+  "/current",
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
+  }
+);
+
 module.exports = router;
