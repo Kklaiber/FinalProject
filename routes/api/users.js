c96e6295f5dd39758ede4fc3b8f149aed8f2212c
Kyle Klaiber <Kylesklaiber@gmail.com>
2018-10-03 11:40:13 -0700

    Validation for Register & Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 //Load User model
 const User = require("../../models/User");
 
@@ -18,9 +22,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users route works!" }));
 //@desc    Register user
 //@access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email Already Exists!" });
+      errors.email = "Email Already Exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //Size
@@ -52,6 +64,13 @@ router.post("/register", (req, res) => {
 //@desc    Login User/ Returning JWT Token
 //@access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -59,7 +78,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     //check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+        errors.email = 'User Not Found';
+      return res.status(404).json(errors);
     }
 
     //check password
@@ -80,7 +100,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password Incorrect" });
+          errors.password = 'Password Incorrect'
+        return res.status(400).json(errors);
       }
     });
   });
@@ -91,12 +112,12 @@ router.post("/login", (req, res) => {
 //@access  Private
 router.get(
   "/current",
-  passport.authenticate('jwt', { session: false }),
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     res.json({
-        id: req.user.id,
-        name: req.user.name,
-        email: req.user.email
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
     });
   }
 );
---
+++ validation/login.js
@@ -0,0 +1,27 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  //Check If required field is empty
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email Is Invalid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email Is Required";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password Is Required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
