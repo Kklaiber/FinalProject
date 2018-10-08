21d3ae595a696c4dd43409a458b0316081c1b3b5
Andrew Talle <talle.andrew@gmail.com>
2018-10-08 13:12:21 -0700

    add Posts & Profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,18 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
   SET_CURRENT_USER
-} from './types';
+} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
   axios
-    .get('/api/profile')
+    .get("/api/profile")
     .then(res =>
       dispatch({
         type: GET_PROFILE,
@@ -27,11 +28,30 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
-    .post('/api/profile', profileData)
-    .then(res => history.push('/dashboard'))
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
@@ -43,26 +63,26 @@ export const createProfile = (profileData, history) => dispatch => {
 // Add experience
 export const addExperience = (expData, history) => dispatch => {
   axios
-    .post('/api/profile/experience', expData)
-    .then(res => history.push('/dashboard'))
-    .catch(err => 
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      })  
+      })
     );
 };
 
 // Add education
 export const addEducation = (eduData, history) => dispatch => {
   axios
-    .post('/api/profile/education', eduData)
-    .then(res => history.push('/dashboard'))
-    .catch(err => 
+    .post("/api/profile/education", eduData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      })  
+      })
     );
 };
 
@@ -70,17 +90,17 @@ export const addEducation = (eduData, history) => dispatch => {
 export const deleteExperience = id => dispatch => {
   axios
     .delete(`/api/profile/experience/${id}`)
-    .then(res => 
+    .then(res =>
       dispatch({
         type: GET_PROFILE,
         payload: res.data
       })
     )
-    .catch(err => 
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      })  
+      })
     );
 };
 
@@ -88,26 +108,45 @@ export const deleteExperience = id => dispatch => {
 export const deleteEducation = id => dispatch => {
   axios
     .delete(`/api/profile/education/${id}`)
-    .then(res => 
+    .then(res =>
       dispatch({
         type: GET_PROFILE,
         payload: res.data
       })
     )
-    .catch(err => 
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      })  
+      })
+    );
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
     );
 };
 
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
-  if(window.confirm('Are you sure? this can NOT be undone')) {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
     axios
-      .delete('/api/profile')
-      .then(res => 
+      .delete("/api/profile")
+      .then(res =>
         dispatch({
           type: SET_CURRENT_USER,
           payload: {}
