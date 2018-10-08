7d9679d1219a78a3618c90510fca4d0de6805637
David Scott Perez <david.scott.perez@live.com>
2018-10-08 14:21:05 -0700

    added react fade-in and some UI changes for collective implemenation

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import FadeIn from 'react-fade-in';
 
 class Landing extends Component {
   componentDidMount() {
@@ -16,21 +17,26 @@ class Landing extends Component {
         <div className="dark-overlay landing-inner text-light">
           <div className="container">
             <div className="row">
+           
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
+              <FadeIn>
+                <h1 className="display-3 mb-4 brand-text">Collective</h1>
                 <p className="lead">
                   {' '}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
+                  <span className="gold-text">Welcome</span> to your communities.
                 </p>
                 <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                <div>
+                <Link to="/register" className="btn btn-lg btn-warning mr-2">
                   Sign Up
                 </Link>
                 <Link to="/login" className="btn btn-lg btn-light">
                   Login
                 </Link>
+                </div>
+                </FadeIn>
               </div>
+             
             </div>
           </div>
         </div>
