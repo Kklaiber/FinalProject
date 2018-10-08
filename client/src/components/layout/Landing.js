bc6b27090ccf89d49923ab3573405367a32a6a3e
Kyle Klaiber <Kylesklaiber@gmail.com>
2018-10-08 15:37:48 -0700

    minor change to html title

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
-import FadeIn from 'react-fade-in';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import FadeIn from "react-fade-in";
 
 class Landing extends Component {
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
+      this.props.history.push("/dashboard");
     }
   }
 
@@ -17,30 +17,28 @@ class Landing extends Component {
         <div className="dark-overlay landing-inner text-light">
           <div className="container">
             <div className="row">
-           
               <div className="col-md-12 text-center">
-
-              <FadeIn> 
-
-                <h1 className="display-3 mb-4 brand-text">Collective</h1>
-                <p className="lead">
-                  {' '}
-                  <span className="gold-text">Welcome</span> to your communities.
-                </p>
-                <hr />
-                <div>
-                <Link to="/register" className="btn btn-lg btn-warning mr-2">
-                  Sign Up
-                </Link>
-                <Link to="/login" className="btn btn-lg btn-light">
-                  Login
-                </Link>
-                </div>
-
+                <FadeIn>
+                  <h1 className="display-3 mb-4 brand-text">Collective</h1>
+                  <p className="lead">
+                    {" "}
+                    <span className="gold-text">Welcome</span> to your
+                    communities.
+                  </p>
+                  <hr />
+                  <div>
+                    <Link
+                      to="/register"
+                      className="btn btn-lg btn-warning mr-2"
+                    >
+                      Sign Up
+                    </Link>
+                    <Link to="/login" className="btn btn-lg btn-light">
+                      Login
+                    </Link>
+                  </div>
                 </FadeIn>
-
               </div>
-             
             </div>
           </div>
         </div>
