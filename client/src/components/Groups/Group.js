b888a913c3d1ab3858b3a7f908764e7d3166050b
Andrew Talle <talle.andrew@gmail.com>
2018-10-12 09:25:01 +0200

    andrew branch

---
+++ client/src/components/Groups/Group.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Group extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Group;
