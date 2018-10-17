ba8fd0de7b7089915cf385f71c3aa17b3d1147a6
Ethan-LeBlond <ehleblond@live.com>
2018-10-17 15:16:40 -0700

    tiny edits

--- client/src/components/add-credentials/AddInterests.js
+++ client/src/components/add-credentials/AddInterests.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import TextFieldGroup from '../common/TextFieldGroup';
-import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+// import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addInterests } from '../../actions/profileActions';
