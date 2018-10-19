9715a544583148f93d59c0311b29177ffa660fb6
Kyle Klaiber <Kylesklaiber@gmail.com>
2018-10-19 15:56:11 -0700

    Set up for deploy

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-const events = require('./routes/api/events');
-const postsmissions = require('./routes/api/postsmissions');
-const postsoutdoors = require('./routes/api/postsoutdoors');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const events = require("./routes/api/events");
+const postsmissions = require("./routes/api/postsmissions");
+const postsoutdoors = require("./routes/api/postsoutdoors");
 
 const app = express();
 
@@ -17,29 +18,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('<<< Hidey Ho Neighbor, your DB is connected >>>'))
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("<<< Hidey Ho Neighbor, your DB is connected >>>"))
   .catch(err => console.log(err, "The database is unable to connect"));
 
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-app.use('/api/events', events);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+app.use("/api/events", events);
 //extra routes for features
-app.use('/api/posts-missions', postsmissions);
+app.use("/api/posts-missions", postsmissions);
 
-app.use('/api/posts-outdoors', postsoutdoors);
+app.use("/api/posts-outdoors", postsoutdoors);
+
+//Server Static Assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
