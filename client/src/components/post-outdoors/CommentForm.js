bcc8cad3250c6b8b37f47aeb2986f30a67f4b607
Kyle Klaiber <Kylesklaiber@gmail.com>
2018-11-02 13:16:35 -0700

    fixed comment ability for missions and outdoors

--- client/src/components/post-outdoors/CommentForm.js
+++ client/src/components/post-outdoors/CommentForm.js
@@ -31,7 +31,7 @@ class CommentForm extends Component {
     const newComment = {
       text: this.state.text,
       name: user.name,
-      avatar: user.avatar
+      avatar: user._id
     };
 
     this.props.addComment(postId, newComment);
