bbbcd6115797ceabb0ef6f2510e1f1ebe8ecf45f
David Scott Perez <david.scott.perez@live.com>
2018-11-05 10:18:13 -0800

    delete edu int andgroups

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,9 +5,9 @@ import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 import ProfileActions from "./ProfileActions";
-// import Experience from "./Experience";
-// import Education from "./Education";
-// import Group from "./Group";
+import Experience from "./Experience";
+import Education from "./Education";
+import Group from "./Group";
 import PopupModal from './Modal';
 import HelpModal from './HelpModal';
 import Posts from "../posts-dashboard/Posts"
@@ -41,9 +41,9 @@ class Dashboard extends Component {
               <br/>
             <ProfileActions />
 
-            {/* <Experience experience={profile.experience} />
+            <Experience experience={profile.experience} />
             <Education education={profile.education} />
-            <Group group={profile.group} /> */}
+            <Group group={profile.group} />
             <HelpModal />
             <div style={{ marginBottom: "60px" }} />
           </div>
